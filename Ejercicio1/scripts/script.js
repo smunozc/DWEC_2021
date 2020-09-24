e6253ae41c2bc5dffb46b8554356733f5af83b97
salvi <salvador.munoz0109@gmail.com>
2020-09-24 23:19:56 +0200

    Ejercicio1 JavaScript

---
+++ Ejercicio1/scripts/script.js
@@ -0,0 +1,15 @@
+//U1T1 - Números Salvador Muñoz Cordero
+
+//Variables
+var entero = 1357;
+var decimal = 1357.0;
+var cientifico = 135e7;
+var octal = 01357;
+var hexadecimal = 0x1357;
+
+//Mensajes mostrando el valor de las variables
+alert("Número entero" + entero);
+alert("Número decimal" + decimal);
+alert("Número cientifico" + cientifico);
+alert("Número octal" + octal);
+alert("Número hexadecimal" + hexadecimal);
