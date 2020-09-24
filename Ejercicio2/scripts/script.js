5f9230601bf2cfeb79fa4fa090d8fd1549f5d828
salvi <salvador.munoz0109@gmail.com>
2020-09-24 21:43:00 +0200

    Ejercicio 2 JavaScript

---
+++ Ejercicio2/scripts/script.js
@@ -0,0 +1,14 @@
+//U1T2 - Cadenas - Salvador Muñoz Cordero
+
+//Variables
+var cadena1 = "Hola";
+var cadena2 = "7";
+var cadena3 = "13";
+var cadena4 = "Adios";
+
+//Mensajes mostrando el valor de las variables
+alert("frase uno");
+alert(cadena1 + "\n" + cadena4);
+alert(cadena2+cadena3);
+alert(cadena1+cadena2+cadena3+cadena4);
+alert("Número hexadecimal" + hexadecimal);
