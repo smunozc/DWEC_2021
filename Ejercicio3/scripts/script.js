6cbcdfe4cced91b858d8c208ac7ae5c8652a72ec
salvi <salvador.munoz0109@gmail.com>
2020-09-25 18:10:44 +0200

    Ejercicio 3 JavaScript

---
+++ Ejercicio3/scripts/script.js
@@ -0,0 +1,22 @@
+//U1T2 - Cadenas - Salvador Muñoz Cordero
+
+//Variables
+var operacion1 = 10 == 10;
+var operacion2 = 10 === 10;
+var operacion3 = 10 === 10.0;
+var operacion4 = "Laura" == "laura";
+var operacion5 = "Laura" > "laura";
+var operacion6 = "Laura" < "laura";
+var operacion7 = "123" == 123;
+var operacion8 = "123" === 123;
+var operacion9 = parseInt("123") === 123;
+
+//Mensajes mostrando el valor de las variables
+alert("La operación 10 == 10 es" + operacion1);
+alert("La operación 10 == 10 es" + operacion2);
+alert("La operación 10 == 10 es" + operacion3);
+alert("La operación 10 == 10 es" + operacion4);
+alert("La operación 10 == 10 es" + operacion6);
+alert("La operación 10 == 10 es" + operacion7);
+alert("La operación 10 == 10 es" + operacion8);
+alert("La operación 10 == 10 es" + operacion9);
