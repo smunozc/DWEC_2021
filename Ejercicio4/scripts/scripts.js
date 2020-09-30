b3b07633444a855ca50446d3ffba9bfe1f539943
salvi <salvador.munoz0109@gmail.com>
2020-09-30 20:08:30 +0200

    Tarea 4 Comparacion II

---
+++ Ejercicio4/scripts/scripts.js
@@ -0,0 +1,27 @@
+//U1T4 - Comparación II - Salvador Muñoz Cordero
+
+//Variables
+var operacion1 = null == undefined;
+var operacion2 = "NaN" == NaN;
+var operacion3 = 5 == NaN;
+var operacion4 = NaN == NaN;
+var operacion5 = NaN != NaN;
+var operacion6 = false == 0;
+var operacion7 = true == 1;
+var operacion8 = true == 2;
+var operacion9 = undefined == 0;
+var operacion10 = null == 0;
+var operacion11 = "5" == 5;
+
+//Mensajes mostrando el valor de las variables
+alert("La operación (null == undefined) es" + " " + operacion1);
+alert("La operación ('NaN' == NaN) es" + " " + operacion2);
+alert("La operación (5 == NaN) es" + " " + operacion3);
+alert("La operación (NaN == NaN) es" + " " + operacion4);
+alert("La operación (NaN != NaN) es" + " " + operacion5);
+alert("La operación (false == 0) es" + " " + operacion6);
+alert("La operación (true == 1) es" + " " + operacion7);
+alert("La operación (true == 2) es" + " " + operacion8);
+alert("La operación (undefined == 0) es" + " " + operacion8);
+alert("La operación (null == 0) es" + " " + operacion8);
+alert("La operación ('5' == 5) es" + " " + operacion8);
