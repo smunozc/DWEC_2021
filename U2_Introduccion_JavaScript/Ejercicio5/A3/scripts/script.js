58a08588c430a628b46fd511e86737d704d5ca05
salvi <salvador.munoz0109@gmail.com>
2020-10-02 17:31:55 +0200

    Ejercicio 5 JavaScript

--- U2_Introduccion_JavaScript/Ejercicio5/A3/scripts/script.js
+++ U2_Introduccion_JavaScript/Ejercicio5/A3/scripts/script.js
@@ -1,8 +1,18 @@
 var x = 1;
-while(x<500){
-    if(x%3 === 0 || x){
+var multiplos3 = 0;
+var noPrimos = 0;
 
+while(x<500){
+    if(x%3 === 0 || x%2 === 0){
+        if(x%3 === 0){
+            multiplos3++;
+        }
+        //El calculo de los no primos es erróneo
+        if(x%2 === 0){
+            noPrimos++;
+        }
     }
     x++;
 }
+console.log("Cantidad de numeros multiplos de 3: " + multiplos3 + '\n' + "Cantidad de numeros no primos: " + noPrimos);
 
