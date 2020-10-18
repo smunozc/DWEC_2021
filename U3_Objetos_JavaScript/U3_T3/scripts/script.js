88289934aa0c073ef63f5044dfa6be6526d4eb79
salvi <salvador.munoz0109@gmail.com>
2020-10-18 21:52:05 +0200

    U3 T3

--- U3_Objetos_JavaScript/U3_T3/scripts/script.js
+++ U3_Objetos_JavaScript/U3_T3/scripts/script.js
@@ -1,4 +1,33 @@
-function numero(){
-    let num = document.getElementById("num");
-    let cuatroDecimales
+function numero() {
+
+    let num = parseInt(document.getElementById("num").value);
+
+    if (num != null) {
+
+        console.log("No es nulo");
+
+        if (!isNaN(num)) {
+
+            console.log("Es número");
+
+            if (Number.isInteger(num)) {
+
+                console.log("Es entero");
+
+                let cuatroDecimales = Number(num).toFixed(4);;
+                let binario = Number(num).toString(2);
+                let octal = Number(num).toString(8);
+                let hexadecimal = Number(num).toString(16);
+
+                alert("Resultado: " + '\n' +
+                    "Cuatro decimales: " + cuatroDecimales + '\n' +
+                    "Binario: " + binario + '\n' +
+                    "Octal: " + octal + '\n' +
+                    "Hexadecimal: " + hexadecimal);
+
+            }
+        }
+    }
+
+
 }
