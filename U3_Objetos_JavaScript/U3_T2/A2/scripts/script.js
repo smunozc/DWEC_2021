9ee288fbde840e33e173174360bb686cc89a7165
salvi <salvador.munoz0109@gmail.com>
2020-10-18 21:00:51 +0200

    Terminado U3 T2

--- U3_Objetos_JavaScript/U3_T2/A2/scripts/script.js
+++ U3_Objetos_JavaScript/U3_T2/A2/scripts/script.js
@@ -1,35 +1,17 @@
-//Terminar ejercicio
 function mathOperations() {
-    let formatType = parseInt(document.getElementById("radius").value);
+    let pi = Math.PI;
+    let radio = parseInt(document.getElementById("radius").value);
+    let diametro = radio * 2;
+    let perimetro = 2 * pi * radio;
+    let areaCircunferencia = pi * Math.pow(radio,2);
+    let areaEsfera = 4 * pi * Math.pow(radio,2);
+    let volumenEsfera = (4 * pi * Math.pow(radio,3)) / 3;
 
-    switch (formatType) {
-        case 1:
-            let base = parseInt(prompt("Introduce un numero base"));
-            let exponente = prompt("Introduce un numero de exponente");
-            alert("La potencia de " + base + " elevado a " + exponente + " es: " + Math.pow(base, exponente));
-            break;
-        case 2:
-            let raiz = parseInt(prompt("Introduce un numero positivo para calcular su raiz cuadrada."));
-            if(raiz >= 0){
-                alert("La raíz de " + raiz + " es: " + Math.sqrt(raiz));
-            } else {
-                alert("Debe ser un número positivo.");
-            }
-            break;
-        case 3:
-            let redondeo = parseFloat(prompt("Introduce un número decimal para redondearlo."));
-            alert("Redondeo a la alta: " + Math.ceil(redondeo) + '\n' + "Redondeo a la baja: " + Math.floor(redondeo));
-            break;
-        case 4:
-            let angulo = parseInt(prompt("Introduce un ángulo entre 0 y 360."));
-            let seno = Math.sin(angulo);
-            let coseno = Math.cos(angulo);
-            let tangente = Math.tan(angulo);
-            if(angulo >= 0 && angulo <= 360){
-                alert("Seno: " + seno + '\n' + "Coseno: " + coseno + '\n' + "Tangente: " + tangente);
-            } else {
-                alert("Escriba un numero entre 0 y 360.");
-            }
-            break;
-    }
+    alert("Resultados: " + '\n' +
+    "radio: " + radio + '\n' +
+    "diametro: " + diametro + '\n' +
+    "perimetro: " + perimetro + '\n' +
+    "area circunferencia: " + areaCircunferencia + '\n' +
+    "area esfera: " + areaEsfera + '\n' +
+    "volumen esfera: " + volumenEsfera);
 }
