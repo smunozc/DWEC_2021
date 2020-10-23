c3e35ee0c8a815de7f26fcd53eb2922df2742607
salvi <salvador.munoz0109@gmail.com>
2020-10-23 18:24:26 +0200

    U3 T4

---
+++ U3_Objetos_JavaScript/U3_T5/scripts/main.js
@@ -0,0 +1,96 @@
+var ventana1;
+
+nuevaVentana();
+
+function nuevaVentana() {
+    let pagina = "./pages/page.html";
+    let confirmacion = confirm("Se va a crear una nueva ventana ¿Estas de acuerdo?");
+    console.log(confirmacion);
+    if (confirmacion) {
+        ventana1 = window.open("./pages/page.html", "ventane", "height=80,width=200,left=500,location=no,menubar=no,toolbar=no,resizable=no,scrollbars=yes,status=no,titlebar=yes,top=500");
+    }
+}
+
+function cerrarVentana() {
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.close();
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function moverVentana() {
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.moveBy(10, 10);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function moverAPosicion(){
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.moveTo(100, 100);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function redimensionarVentana(){
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.resizeBy(10, 10);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function redimensionarA(){
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.resizeTo(400, 200);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function moverScrollArriba(){
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.scrollTo(0,0);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
+
+function moverScrollA10(){
+    if (ventana1 !== undefined) {
+        if (!ventana1.closed) {
+            ventana1.scrollTo(0,10);
+        } else {
+            alert("La ventana ya esta cerrada.");
+        }
+    } else {
+        alert("Primero debes crear una ventana");
+    }
+}
